ef7a438d7c9c16cbb975bd3c4ef3599f3e2cf136
Théotime <92151968+Pagiestm@users.noreply.github.com>
2024-03-11 11:08:19 +0100

    feat(database, package.json): add TypeORM and init db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PresentationModule } from './presentation/presentation.module';
 import { DomainModule } from './domain/domain.module';
 import { DataModule } from './data/data.module';
+import { databaseProviders } from './database.providers';
 
 @Module({
   imports: [PresentationModule, DomainModule, DataModule],
   controllers: [],
-  providers: [],
+  providers: [...databaseProviders],
+  exports: [...databaseProviders],
 })
 export class AppModule {}
