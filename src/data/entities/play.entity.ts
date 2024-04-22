8d5e17bcfeaae55e565720f2515ce0ebd6e84ec0
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-04-22 11:09:25 +0200

    feat: play entity

--- src/data/entities/play.entity.ts
+++ src/data/entities/play.entity.ts
@@ -1,13 +1,14 @@
 import {Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn} from "typeorm";
 import {MatchesEntity} from "./matches.entity";
+import {MembersEntity} from "./members.entity";
 
 @Entity("entity")
 export class PlayEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @ManyToMany(type => Members) @JoinTable()
-    members: Members[];
+    @ManyToMany(type => MembersEntity) @JoinTable()
+    members: MembersEntity[];
 
     @ManyToMany(type => MatchesEntity) @JoinTable()
     matches: MatchesEntity[];
