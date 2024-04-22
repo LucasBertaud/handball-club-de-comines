762f816d8037196cea03af37cd8566a33986cbb8
Théotime <92151968+Pagiestm@users.noreply.github.com>
2024-04-22 11:08:03 +0200

    feat(entities): create foreign key between members and news

--- src/data/entities/news.entity.ts
+++ src/data/entities/news.entity.ts
@@ -1,19 +1,23 @@
-import {Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { MembersEntity } from './members.entity';
 
 @Entity('news')
 export class NewsEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({type: 'varchar', length: 100})
-    title: string;
+  @Column({ type: 'varchar', length: 100 })
+  title: string;
 
-    @Column({type: 'varchar', length: 50})
-    image: string;
+  @Column({ type: 'varchar', length: 50 })
+  image: string;
 
-    @Column('text')
-    content: string;
+  @Column('text')
+  content: string;
 
-    @Column('date')
-    publication_date: Date;
+  @Column('date')
+  publication_date: Date;
+
+  @ManyToOne(type => MembersEntity, member => member.news)
+  member: MembersEntity;
 }
