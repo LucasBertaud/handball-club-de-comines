23a95155bbf8ae2024b6731202b71aa10c677749
Théotime <92151968+Pagiestm@users.noreply.github.com>
2024-04-23 14:10:22 +0200

    Update data.module.ts

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -1,30 +1,69 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ArticleService } from './services/article.service';
-import { ArticleEntity } from './entities/article.entity';
-import { AdversaryTeamsService } from './services/adversary_teams.service';
-import { AdversaryTeamsEntity } from './entities/adversary_teams.entity';
+import {Module} from "@nestjs/common";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ArticleService} from "./services/article.service";
+import {ArticleEntity} from "./entities/article.entity";
+import {MembersEntity} from "./entities/members.entity";
+import {MembersService} from "./services/members.service";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: './src/data/databases/hcc.sqlite',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // Utilisez `synchronize: true` uniquement en développement
-    }),
-    TypeOrmModule.forFeature([
-      ArticleEntity,
-      AdversaryTeamsEntity
-    ])
-  ],
-  exports: [
-    ArticleService,
-    AdversaryTeamsService
-  ],
-  providers: [
-    ArticleService, 
-    AdversaryTeamsService
-  ],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "sqlite",
+            database: "./src/data/databases/hcc.sqlite",
+            entities: [__dirname + "/**/*.entity{.ts,.js}"],
+            synchronize: true, // Utilisez `synchronize: true` uniquement en développement
+        }),
+        TypeOrmModule.forFeature([
+            ArticleEntity,
+            MembersEntity,
+            MembersService,
+            AdversaryTeamsEntity
+        ]),
+    ],
+    exports: [
+        ArticleService,
+        MembersService,
+        AdversaryTeamsEntity
+    ],
+    providers: [
+        ArticleService,
+        MembersService,
+        AdversaryTeamsEntity
+    ],
 })
-export class DataModule { }
+export class DataModule {
+}import {Module} from "@nestjs/common";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ArticleService} from "./services/article.service";
+import {ArticleEntity} from "./entities/article.entity";
+import {MembersEntity} from "./entities/members.entity";
+import {MembersService} from "./services/members.service";
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "sqlite",
+            database: "./src/data/databases/hcc.sqlite",
+            entities: [__dirname + "/**/*.entity{.ts,.js}"],
+            synchronize: true, // Utilisez `synchronize: true` uniquement en développement
+        }),
+        TypeOrmModule.forFeature([
+            ArticleEntity,
+            MembersEntity,
+            MembersService,
+            AdversaryTeamsEntity
+        ]),
+    ],
+    exports: [
+        ArticleService,
+        MembersService,
+        AdversaryTeamsEntity
+    ],
+    providers: [
+        ArticleService,
+        MembersService,
+        AdversaryTeamsEntity
+    ],
+})
+export class DataModule {
+}
